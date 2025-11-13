076c363f7844247149980168249f601d374934fd
polllyass <polllya0990@gmail.com>
2025-11-13 23:04:55 +0300

    исправила 1.js, 2.js, 3.js

--- L3/1.js
+++ L3/1.js
@@ -1,17 +1,18 @@
-//1.
-const responseData = pm.response.json();
-function sortByTitleLengthDesc(a, b) {
-    const lengthA = a.title?.length || 0;
-    const lengthB = b.title?.length || 0;
-    return lengthB - lengthA;
-}
-//posts
-
-
-const responseData = pm.response.json();
-function sortByAuthorName(a, b) {
-    const nameA = a.name?.toLowerCase() || "";
-    const nameB = b.name?.toLowerCase() || "";
-    return nameA.localeCompare(nameB);
-}//comments
-
+pm.sendRequest('https://jsonplaceholder.typicode.com/posts', function (err, res) {
+    if (!err) {
+        const sortedPosts = res.json().sort((a, b) => b.title.length - a.title.length);
+        pm.environment.set("sortedPosts", JSON.stringify(sortedPosts));
+        console.log("Posts отсортированы по длине заголовка:", sortedPosts[0].title);
+    }
+});
+pm.sendRequest('https://jsonplaceholder.typicode.com/comments', function (err, res) {
+    if (!err) {
+        const sortedComments = res.json().sort((a, b) => {
+            const nameA = a.name?.toLowerCase() || "";
+            const nameB = b.name?.toLowerCase() || "";
+            return nameA.localeCompare(nameB);
+        });
+        pm.environment.set("sortedComments", JSON.stringify(sortedComments));
+        console.log("Comments отсортированы по имени автора:", sortedComments[0].name);
+    }
+});
--- L3/2.js
+++ L3/2.js
@@ -1,15 +1,18 @@
-
-function filterUserFields(user) {
-    return {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-        email: user.email,
-        phone: user.phone
-    };
-}//users
-
-const todos = pm.response.json();
-const incompleteTodos = todos.filter(todo => !todo.completed);
-pm.response.json = () => incompleteTodos;
-//todos
+pm.sendRequest('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+        const filteredUsers = res.json().map(user => ({
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            phone: user.phone
+        }));
+        pm.environment.set("filteredUsers", JSON.stringify(filteredUsers));
+        console.log("отфильтрованные пользователи:", filteredUsers.length);
+    });
+pm.sendRequest('https://jsonplaceholder.typicode.com/todos')
+    .then(res => {
+        const incompleteTodos = res.json().filter(todo => !todo.completed);
+        pm.environment.set("incompleteTodos", JSON.stringify(incompleteTodos));
+        console.log("незавершенные задачи:", incompleteTodos.length);
+    });
